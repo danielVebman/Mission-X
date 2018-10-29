ab0926d9b2bc2650950831318d979981cb7fc41e
= <daniel.vebman@gmail.com>
2018-10-29 16:26:19 -0400

    Started bookseatsvc

--- Mission X/BookSeatsViewController.swift
+++ Mission X/BookSeatsViewController.swift
@@ -10,5 +10,24 @@ import Foundation
 import UIKit
 
 class BookSeatsViewController: UIViewController {
+    var seatsLayout: [[Bool]] = [
+               [true,  true,  true,  true ],
+               [true,  false, false, true ],
+               [true,  true , true,  false],
+        [true,  true,  true,  false, false, false],
+        [true,  false, true,  false, false, false],
+        [true,  true,  false, false, false, false],
+        [true,  true,  false, false, false, false],
+        [true,  true,  false, false, false, false],
+        [true,  true,  false, false, false, true ],
+        [true,  false, false, false, false, true ],
+        [false, false, false, false, true,  false],
+        [false, false, false, false, false, false],
+        [false, false, false, false, false, false],
+        [false, false, false, false, false, false]
+    ]
     
+    override func viewDidLoad() {
+        
+    }
 }
